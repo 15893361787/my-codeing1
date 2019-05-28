fb9d8e7c9b8b3c1f5653eb89bac6131186611bef
admin <2591255234@qq.com>
2019-05-28 23:39:34 +0800

    promise

--- JavaScript/src/codeing/promise/my-promise.js
+++ JavaScript/src/codeing/promise/my-promise.js
@@ -68,8 +68,12 @@ function resolvePromise(promise, result, resolve, reject) {
 
 myPromise.prototype.then = function (onfilled, onRejected) {
     let self = this;
-onfilled=typeof onfilled=='function'?onfilled:res=>{return res};
-onRejected=typeof onRejected=='function'?onRejected:err=>{throw err;}
+    onfilled = typeof onfilled == 'function' ? onfilled : res => {
+        return res
+    };
+    onRejected = typeof onRejected == 'function' ? onRejected : err => {
+        throw err;
+    }
     let promiseTemp = new myPromise((resolve, reject) => {
         if (self.status == "resolve") {
             try {
