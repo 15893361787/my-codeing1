bc5c6c274c8c3de5d2c8a17f5aaaf90c30aa18e9
admin <2591255234@qq.com>
2019-05-28 20:40:00 +0800

    promise

--- JavaScript/src/codeing/promise/my-promise.js
+++ JavaScript/src/codeing/promise/my-promise.js
@@ -2,28 +2,39 @@ function myPromise(excutor) {
     let self = this;
     self.status = "pending"
     self.value = undefined;
+    self.onResolveCallbacks=[];
+    self.onRejectedCallbacks=[];
     function resolve(value) {
         if (self.status =="pending") {
             self.status = "resolve";
             self.value = value;
+            self.onResolveCallbacks.forEach((fun)=>{
+                fun.call(null,self.value);
+            })
         }
     }
     function reject(reason) {
         if (self.status == "pending") {
             self.status = "rejected";
             self.value = reason;
+            self.onRejectedCallbacks.forEach((fun)=>{
+                fun.call(null,self.value);
+            })
         }
     }
     excutor(resolve, reject);
 }
 myPromise.prototype.then = function (onfilled, onRejected) {
     let self = this;
-    console.log(self);
     if (self.status == "resolve") {
         onfilled(self.value);
     }
     if (self.status =="rejected") {
         onRejected(self.value);
+    }
+    if (self.status="pending"){
+        self.onResolveCallbacks.push(onfilled);
+        self.onRejectedCallbacks.push(onRejected);
     }
 }
 
