70a747f5de521dd3d14d7015ac2cdc33a9ef599d
admin <2591255234@qq.com>
2019-05-28 19:55:48 +0800

    promise

--- JavaScript/src/codeing/promise/index.js
+++ JavaScript/src/codeing/promise/index.js
@@ -1,11 +1,5 @@
-new Promise(
-    (resolve, reject) => {
-        resolve(111);
-    }).then(
-    (res) => {
-        console.log(res);
-    }).catch(
-    (err) => {
-        console.log(err);
-    }
+let myPromise = require('./my-promise');
+new myPromise((resolve, reject) => {resolve(111);})
+    .then((res) => {console.log(res);},
+       (err) => {console.log(err);}
 )
--- JavaScript/src/codeing/promise/my-promise.js
+++ JavaScript/src/codeing/promise/my-promise.js
@@ -1,11 +1,34 @@
 function myPromise(excutor) {
-    function resolve() {
+    let self = this;
+    self.status = "pending"
+    self.value = undefined;
+
+    function resolve(value) {
+        if (self.status = "pending") {
+            self.status = "resolve";
+            self.value = value;
+        }
 
     }
 
-    function reject() {
+    function reject(reason) {
+        if (self.status == "pending") {
+            self.status = "rejected";
+            self.value = reason;
+        }
 
     }
 
-    excutor.call(this,resolve,reject);
+    excutor(resolve, reject);
 }
+
+myPromise.prototype.then = function (onfilled, onRejected) {
+    let self = this;
+    if (self.status == "resolve") {
+        onfilled(self.value);
+    }
+    if (self.status =="rejected") {
+        onRejected(self.value);
+    }
+}
+module.exports=myPromise;
