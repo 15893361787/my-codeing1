ec28a0a8b48761be7a168219b16e49553eb9ee91
admin <2591255234@qq.com>
2019-05-28 21:14:09 +0800

    promise

--- JavaScript/src/codeing/promise/index.js
+++ JavaScript/src/codeing/promise/index.js
@@ -1,13 +1,12 @@
 let myPromise = require('./my-promise');
 let promise1 = new myPromise((resolve, reject) => {
-    setTimeout(() => {
-        reject('成功')
-    }, 1000)
+    resolve('成功')
 });
 promise1.then((res) => {
-        console.log(res);
+        return '再次成功'
     },
     (err) => {
         console.log(err);
-    }
-)
+    }).then(res => {
+    console.log(res);
+})
--- JavaScript/src/codeing/promise/my-promise.js
+++ JavaScript/src/codeing/promise/my-promise.js
@@ -2,46 +2,81 @@ function myPromise(excutor) {
     let self = this;
     self.status = "pending"
     self.value = undefined;
-    self.onResolveCallbacks=[];
-    self.onRejectedCallbacks=[];
+    self.onResolveCallbacks = [];
+    self.onRejectedCallbacks = [];
+
     function resolve(value) {
-        if (self.status =="pending") {
+        if (self.status == "pending") {
             self.status = "resolve";
             self.value = value;
-            self.onResolveCallbacks.forEach((fun)=>{
-                fun.call(null,self.value);
+            self.onResolveCallbacks.forEach((fun) => {
+                fun()
             })
         }
     }
+
     function reject(reason) {
         if (self.status == "pending") {
             self.status = "rejected";
             self.value = reason;
-            self.onRejectedCallbacks.forEach((fun)=>{
-                fun.call(null,self.value);
+            self.onRejectedCallbacks.forEach((fun) => {
+                fun()
             })
         }
     }
+
     excutor(resolve, reject);
+}
+function resolvePromise(promise,result,resolve,reject){
+    if (result instanceof myPromise){
+        return result;
+    }
 }
 myPromise.prototype.then = function (onfilled, onRejected) {
     let self = this;
-    if (self.status == "resolve") {
-        onfilled(self.value);
-    }
-    if (self.status =="rejected") {
-        onRejected(self.value);
-    }
-    if (self.status="pending"){
-        self.onResolveCallbacks.push(onfilled);
-        self.onRejectedCallbacks.push(onRejected);
-    }
+    let promiseTemp = new myPromise((resolve, reject) => {
+        if (self.status == "resolve") {
+            try {
+                let result = onfilled(self.value);
+                resolvePromise(promiseTemp,result,resolve,reject);
+            } catch (e) {
+                reject(e);
+            }
+
+        }
+        if (self.status == "rejected") {
+            try {
+                let result = onRejected(self.value);
+                resolvePromise(promiseTemp,result,resolve,reject);
+            } catch (e) {
+                reject(e);
+            }
+
+        }
+        if (self.status = "pending") {
+            self.onResolveCallbacks.push(() => {
+                try {
+                    let result = onfilled(self.value);
+                    resolvePromise(promiseTemp,result,resolve,reject);
+                } catch (e) {
+                    reject(e)
+                }
+
+            });
+            self.onRejectedCallbacks.push(() => {
+                try {
+                    let result = onRejected(self.value);
+                    resolvePromise(promiseTemp,result,resolve,reject);
+                } catch (e) {
+                    reject(e);
+                }
+
+            });
+        }
+    });
+    return promiseTemp;
+
 }
 
 
-
-
-
-
-
-module.exports=myPromise;
+module.exports = myPromise;
