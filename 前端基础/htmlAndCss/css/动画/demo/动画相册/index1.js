39a75e614c873285565db5f566a0fa123e803c86
2591255234@qq.com <woaini@521>
2020-08-29 21:59:57 +0800

    '动画文件代码修改'

---
+++ 前端基础/htmlAndCss/css/动画/demo/动画相册/index1.js
@@ -0,0 +1,22 @@
+/**
+ * @description 页面加载的时候进行图片添加旋转动画
+ * */
+window.onload = function () {
+  const imgList = document.querySelector(".img-list");
+  const imgs = document.querySelectorAll(".img-list>img");
+  const imgLength = imgs.length;
+  const imgRotateDeg = 360 / imgLength;
+  imgs.forEach((imgElement, index) => {
+    imgElement.style.transform = `rotateY(${
+      imgRotateDeg * index
+    }deg) translateZ(300px)  `;
+    imgElement.style.transition = `1.5s linear ${
+      (imgLength - index - 1) * 0.2
+    }s`;
+  });
+  let num = 0;
+  setTimeout(function () {
+    num++;
+    imgList.style.transform = ` rotateX(-10deg) rotateY(${num * 0.2}deg)  `;
+  }, 30);
+};
